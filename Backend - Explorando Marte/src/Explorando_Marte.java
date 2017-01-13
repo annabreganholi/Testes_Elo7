aa13800d8e304576c67518bf48a99edb0e32a2ae
Anna Breganholi <annabreganholi@hotmail.com>
2017-01-13 20:42:05 -0200

    File reader implemented

--- Backend - Explorando Marte/src/Explorando_Marte.java
+++ Backend - Explorando Marte/src/Explorando_Marte.java
@@ -1,5 +1,25 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Explorando_Marte {
-	public static void main (String []args){
+	public static Map map;	
+	public static void main (String []args) throws IOException{
 		
+		FileReader freader=new FileReader("input.txt");
+	    BufferedReader inputFile=new BufferedReader(freader);
+		String line=null;
+	    List<String> input = new ArrayList<>();
+	    while ((line = inputFile.readLine())!= null) 
+	    	input.add(line);
+	    
+	    
+	        for (int i=0; i<input.size();i++)
+	    System.out.println(input.get(i));    
+	    //map = new Map();
+	        inputFile.close();
 	}
 }
--- Backend - Explorando Marte/src/Map.java
+++ Backend - Explorando Marte/src/Map.java
@@ -1,9 +1,15 @@
 
 public class Map {
 	static int[][] mapSize;
+	static int maxX;
 	
 	public Map (int X, int Y){
 		Map.mapSize = new int[X+1][Y+1]; // +1 porque a coordena do ponto superior direito sera um a mais que a posicao maxima do array
+		Map.maxX = X;
+	}
+	
+	public int getMapX(){
+		return maxX;
 	}
 	
 }
