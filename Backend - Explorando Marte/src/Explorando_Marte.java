746ae9446cacfe72337376d32ad79dfc4f818b6e
Anna Breganholi <annabreganholi@hotmail.com>
2017-01-13 22:18:20 -0200

    Wrong output

--- Backend - Explorando Marte/src/Explorando_Marte.java
+++ Backend - Explorando Marte/src/Explorando_Marte.java
@@ -6,20 +6,63 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Explorando_Marte {
-	public static Map map;	
+	public static Map map;
+	static List<String> input = new ArrayList<>();
+	
 	public static void main (String []args) throws IOException{
+		
+		Sonda sonda;
+		String[] sondaInitialPosition;
+		List<String> output = new ArrayList<>();
+		String commands;
 		
 		FileReader freader=new FileReader("input.txt");
 	    BufferedReader inputFile=new BufferedReader(freader);
 		String line=null;
-	    List<String> input = new ArrayList<>();
+	    
 	    while ((line = inputFile.readLine())!= null) 
 	    	input.add(line);
 	    
+	    //Criando mapa
+	    System.out.println(input.get(0).charAt(0));
+	    map = new Map(Character.getNumericValue(input.get(0).charAt(0)), 
+	    				Character.getNumericValue(input.get(0).charAt(2)));
 	    
-	        for (int i=0; i<input.size();i++)
-	    System.out.println(input.get(i));    
-	    //map = new Map();
-	        inputFile.close();
+	    
+	    for (int i=1; i<input.size();i++){
+	    	//Criando Sonda e colocando posicao inicial
+	    	sondaInitialPosition = input.get(i).split(" ");
+	    	
+	    	//Pegando primeiro e segundo valor do array de String, passando os dois primeiros para coordenadas, e o terceiro para char (Direcao)
+	    	sonda = new Sonda(Integer.parseInt(sondaInitialPosition[0]), 
+	    			Integer.parseInt(sondaInitialPosition[1]), 
+	    			sondaInitialPosition[2].charAt(0));
+	    	System.out.println(sonda.initialPosition[0] + " "+ sonda.initialPosition [1] + " "+ sonda.direction);
+	    	i++;
+	    	
+	    	//Realizando operacoes de caminho
+	    	commands = input.get(i);
+	    	
+	    	for (int j = 0; j < input.get(i).length() ; j++){
+	    		if (commands.charAt(j) == 'L' || commands.charAt(j) == 'R'){
+	    			sonda.changeDirection(commands.charAt(j));
+	    		}else if (commands.charAt(j) == 'M'){
+	    			sonda.move();
+	    		}
+	    		
+	    	}
+	    	
+	    	//Salvando as posicoes finais
+	    	output.add((map.getMapX() - sonda.actualPosition[0])+ " " + 
+	    								sonda.actualPosition[1] + " " + 
+	    								sonda.direction);
+
+	    }
+	    
+	    for (int i = 0; i < output.size() ; i++){
+	    	System.out.println(output.get(i));
+	    }
+	   
+	    inputFile.close();
 	}
 }
--- Backend - Explorando Marte/src/Sonda.java
+++ Backend - Explorando Marte/src/Sonda.java
@@ -13,11 +13,38 @@ public class Sonda {
 	}
 	
 	public void changeDirection (char newDirection){
-		this.direction = newDirection;
+		if (this.direction == 'N'){
+			System.out.println('N');
+			if (newDirection == 'L') this.direction = 'W';
+			else if (newDirection == 'R') this.direction = 'E';
+		}else if (this.direction == 'S'){
+			System.out.println('S');
+			if (newDirection == 'L')this.direction = 'E';
+			else if (newDirection == 'R') this.direction = 'W';
+		}else if (this.direction == 'E'){
+			System.out.println('E');
+			if (newDirection == 'L') this.direction = 'N';
+			else if (newDirection == 'R') this.direction = 'S';
+		}else if (this.direction == 'W'){
+			System.out.println('W');
+			if (newDirection == 'L') this.direction = 'S';
+			else if (newDirection == 'R') this.direction = 'N';
+		}
 	}
 	
-	public void changeActualPosition(){
-		
+	public void move(){
+		if(direction == 'N'){
+			this.actualPosition[0] = this.actualPosition[0] - 1; //Y mantem, X diminui 1 (numeracao do X esta invertido em relacao ao plano cartesiano da atividade
+		}
+		else if(direction == 'S'){
+			this.actualPosition[0] = this.actualPosition[0] + 1; //Y mantem, X aumenta 1
+		}
+		else if(direction == 'E'){
+			this.actualPosition[1] = this.actualPosition[1] + 1; //X mantem, Y aumenta 1
+		}
+		else if(direction == 'W'){
+			this.actualPosition[1] = this.actualPosition[1] - 1; //X mantem, Y diminui 1
+		}
 	}
 	
 }
