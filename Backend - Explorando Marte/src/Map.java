e22b78291669ad165371ee5706275f2e1f650e87
Anna Breganholi <annabreganholi@hotmail.com>
2017-01-16 10:37:46 -0200

    Colors and styles settled, need to align images and header img with text

--- Backend - Explorando Marte/src/Map.java
+++ Backend - Explorando Marte/src/Map.java
@@ -3,15 +3,15 @@ import java.util.List;
 
 public class Map {
 	static int[][] mapSize;
-	static int maxX;
+	static int maxY;
 	
 	public Map (int X, int Y){
 		Map.mapSize = new int[X+1][Y+1]; // +1 porque a coordena do ponto superior direito sera um a mais que a posicao maxima do array
-		Map.maxX = X;
+		Map.maxY = Y;
 	}
 	
 	public int getMapX(){
-		return maxX;
+		return maxY;
 	}
 	public static Map createMap(List<String> input){
 		return new Map(Character.getNumericValue(input.get(0).charAt(0)), 
