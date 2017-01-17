e06d1fb59addebc5be988a2a895a078cb00be788
Anna Breganholi <annabreganholi@hotmail.com>
2017-01-17 09:34:46 -0200

    Minor adjustments in Map

--- Backend - Explorando Marte/src/Map.java
+++ Backend - Explorando Marte/src/Map.java
@@ -6,7 +6,7 @@ public class Map {
 	static int maxY;
 	
 	public Map (int X, int Y){
-		Map.mapSize = new int[X+1][Y+1]; // +1 porque a coordena do ponto superior direito sera um a mais que a posicao maxima do array
+		Map.mapSize = new int[Y+1][X+1]; // +1 porque a coordena do ponto superior direito sera um a mais que a posicao maxima do array
 		Map.maxY = Y;
 	}
 	
